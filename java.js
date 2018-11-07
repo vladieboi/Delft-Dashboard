bef1dc2f2996c5a44a0e1b15924633620a37e0be
vladieboi <vladieboi@gmail.com>
2018-11-07 17:49:24 +0100

    Master file
    Working navigation, bugged F5 Map

---
+++ java.js
@@ -0,0 +1,187 @@
+/* --- MAP DISPLAY */
+var greenLayer;
+var greenLayerOn = false;
+
+var bikeLayer;
+var bikeLayerOn = false;
+
+var trafficLayer;
+var trafficLayerOn = false;
+
+var navContainer = document.getElementById("myDiv");
+var btns = navContainer.getElementsByClassName("navlist")
+
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 52.011, lng: 4.357},
+    	zoom: 13,
+    	disableDefaultUI: true,
+	});
+	greenLayer = new google.maps.Data();
+	bikeLayer = new google.maps.BicyclingLayer(); 
+	trafficLayer = new google.maps.TrafficLayer();  
+}
+
+
+function greenOnOff() {
+    if (greenLayerOn == false){
+        greenLayer.loadGeoJson('http://data-delft.opendata.arcgis.com/datasets/8ec052576bd94271a354bddf6eccd287_1.geojson');
+        greenLayer.setStyle({
+            fillColor: 'green',
+            strokeColor: 'green', 
+            strokeWeight: 1
+        });
+        greenLayer.setMap(map); 
+        greenLayerOn = true;
+        document.getElementById("GreenButton").value="Greenery: ON"
+    } else if (greenLayerOn == true){
+        greenLayer.setMap(null);
+        greenLayerOn = false;
+        document.getElementById("GreenButton").value="Greenery: OFF"
+    }      
+}
+/*
+function bikeOnOff(){
+	if (bikeLayerOn == false){
+		bikeLayer.setMap(map);
+		bikeLayerOn == true;
+		document.getElementById("BikeButton").value="Bike Tracks: ON"
+	}
+	if (bikeLayer == true){
+		bikeLayer.setMap(null);
+		bikeLayerOn == false;
+		document.getElementById("BikeButton").value="Bike Tracks: OFF"
+	}
+}
+
+function trafficOnOff(){
+	if (trafficLayerOn == false){
+		trafficLayer.setMap(map);
+		trafficLayerOn == true;
+		document.getElementById("TrafficButton").value="Traffic: ON"
+	}
+	if (trafficLayer == true){
+		trafficLayer.setMap(null);
+		trafficLayerOn == false;
+		document.getElementById("TrafficButton").value="Traffic: OFF"
+	}
+}
+*/
+
+function bikeOnOff(){
+	bikevalue = document.getElementById('BikeButton').value;
+	greenvalue = document.getElementById('GreenButton').value;
+	trafficvalue = document.getElementById('TrafficButton').value;
+	if(bikevalue == "Bike Tracks: OFF"){
+		document.getElementById("BikeButton").value="Bike Tracks: ON"
+		displayBikes();
+	} else {
+		document.getElementById("BikeButton").value="Bike Tracks: OFF"
+		initMap();
+		if(greenvalue == "Greenery: ON"){
+			displayGreen();
+		}
+		if(trafficvalue == "Traffic: ON"){
+			displayTraffic();
+		}
+	}
+}
+function trafficOnOff(){
+	trafficvalue = document.getElementById('TrafficButton').value;
+	greenvalue = document.getElementById('GreenButton').value;
+	bikevalue = document.getElementById('BikeButton').value;
+	if(trafficvalue == "Traffic: OFF"){
+		document.getElementById("TrafficButton").value="Traffic: ON"
+		displayTraffic();
+	} else {
+		document.getElementById("TrafficButton").value="Traffic: OFF"
+		initMap();
+		if(bikevalue == "Bike Tracks: ON"){
+			displayBikes();
+		}
+		if(greenvalue == "Greenery: ON"){
+			displayGreen();
+		}
+	}
+}
+
+/* --- STYLE FUNCTIONS --- */
+function styleGreen(){
+	map.data.setStyle({
+	  fillColor: 'green',
+	  strokeColor: 'green',
+	  strokeWeight: 1
+	});
+}
+
+/* --- FUNCTION LAYERS --- */
+function displayGreen(){
+	map.data.loadGeoJson(
+    'http://data-delft.opendata.arcgis.com/datasets/8ec052576bd94271a354bddf6eccd287_1.geojson');
+    styleGreen();
+}
+     
+function displayBikes() {
+	var bikeLayer = new google.maps.BicyclingLayer();
+	bikeLayer.setMap(map);
+}
+
+function displayTraffic() {
+	var trafficLayer = new google.maps.TrafficLayer();
+	trafficLayer.setMap(map);
+}
+
+function displayBuildings() {
+	var WMSLayer = new google.maps.ImageMapType({
+    	getTileUrl: function (coord, zoom) {
+        	var proj = map.getProjection();
+        	var zfactor = Math.pow(2, zoom);
+
+        	// get Long Lat coordinates
+        	var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
+        	var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
+
+        	//create the Bounding box string
+        	var bbox =     top.lng() + "," +
+                       bot.lat() + "," +
+                       bot.lng() + "," +
+                       top.lat();
+
+        	//base WMS URL
+        	var url = "http://geodata.nationaalgeoregister.nl/wijkenbuurten2014/wms?";
+            	url += "REQUEST=GetMap&SERVICE=WMS&VERSION=1.1.1&SRS=EPSG:4326&BBOX=" + bbox + "&WIDTH=256&HEIGHT=256";
+            	url += "&LAYERS=" + "cbs_buurten_2014";
+           	    url += "&STYLES=" + "wijkenbuurten_thema_buurten_gemeentewijkbuurt_gemiddeld_aantal_autos_per_huishouden";
+           	    url += "&FORMAT=image/png" ;
+            	url += "&TRANSPARENT=TRUE";
+        	return url;
+    	},
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true
+	});
+
+map.overlayMapTypes.push(WMSLayer);
+}
+
+function show(elementID) {
+	var home = document.getElementById('side-home');
+	home.style.display = 'none';
+	var ele = document.getElementById(elementID); // in case the user clicks on mobility --> getting 'side-mobility' ---- executing -> show('side-mobility');
+	var pages = document.getElementsByClassName('side');
+	for(var i = 0; i < pages.length; i++) {
+		pages[i].style.display = 'none';
+	}
+	ele.style.display = 'flex';
+
+}
+
+function active(elementID) {
+	var x = document.getElementsByClassName("navlistActive");
+	var i;
+	for (i = 0; i < x.length; i++) {
+		x[i].classList.remove("navlistActive");
+	}
+	var active = document.getElementById(elementID);
+	active.classList.toggle("navlistActive");
+}
